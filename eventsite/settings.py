fdc9dabdee844d60c8162622ab2fcfa50693d9c4
giacomo <giacomo.vinchesi@gmail.com>
2025-07-09 17:34:17 +0200

    fixed settings

--- eventsite/settings.py
+++ eventsite/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'eventsite.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ['DATABASE_URL'],
         conn_max_age=600,
         conn_health_checks=True,
     )
